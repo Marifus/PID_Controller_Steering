706b677f007e259137b5f1a213cf0cb5d0945091
Marifus <mehmetariffy@gmail.com>
2025-08-03 21:05:49 +0300

    Adaptive PID

--- include/pid_pkg/controller.hpp
+++ include/pid_pkg/controller.hpp
@@ -12,6 +12,7 @@
 #include <tf2/LinearMath/Transform.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <std_msgs/Bool.h>
+#include <deque>
 
 namespace controller {
 
@@ -30,16 +31,28 @@ namespace controller {
 
         int wp_index, ctrl_index;
         int marker_id;
-        double Kp, Ki, Kd, current_heading;
-        double wheelbase, ctrl_max;
-        double velocity;
+        double Kp, Ki, Kd, Kp_max, Ki_max, Kd_max, Kp_min, Ki_min, Kd_min;
+        double current_heading, current_velocity;
+        double wheelbase;
+        double high_ctrl_max, low_ctrl_max, ctrl_max;
+        double max_velocity, high_velocity, low_velocity, velocity;
         std::string odom_topic, path_topic, cmd_topic, marker_topic, docking_signal_topic;
         double herhangi_bir_sey;
         bool docking, path_received;
+        int filter_length;
+        double weight_current;
+        double curvature_coefficient, curvature_lookahead, max_curvature;
+        int curvature_index;
+
+        std::deque<double> prev_inputs_deq;
         
-        double d_error;
-        double i_error;
-        double prev_error;
+        double steering_prev_error;
+        double steering_i_error;
+        double steering_d_error;
+
+        double velocity_prev_error;
+        double velocity_i_error;
+        double velocity_d_error;
 
         double prev_time;
         double current_time;
@@ -56,13 +69,16 @@ namespace controller {
         void DockingCallback(const std_msgs::Bool& msg);
         void TimerCallback(const ros::TimerEvent&);
         void ControlOutput();
-        double PID(double error, double t_Kp, double t_Ki, double t_Kd, double dt);
-        double UpdateError(const geometry_msgs::Pose& target_point_pose, const geometry_msgs::Pose& current_point_pose);
+        double PID(double error, double t_Kp, double t_Ki, double t_Kd, double dt, double& prev_error, double& i_error, double& d_error);
+        double UpdateSteeringError(const geometry_msgs::Pose& target_point_pose, const geometry_msgs::Pose& current_point_pose);
         geometry_msgs::PoseStamped ChooseWaypoint(const geometry_msgs::Pose& current_point_pose, const nav_msgs::Path& t_path, int ctrl_idx, double ctrl_max);
         int ClosestWaypointIndex(const geometry_msgs::Pose& current_pose, const nav_msgs::Path& t_path);
         geometry_msgs::Pose LocalTransform(const geometry_msgs::Pose& origin_pose, const geometry_msgs::Pose& target_point_pose);
         double GetYaw(const geometry_msgs::Quaternion& q);
-
+        double CalculatePathCurv(const geometry_msgs::Pose& current_point_pose,const nav_msgs::Path& t_path, int curv_idx, double t_curv_lookahead, double t_max_curv);
+        double CalculateVelocityCmd(double velocity, double curvature, double k_curvature);
+        double LowPassFilter(double input, std::deque<double> &prev_inputs, int f_length, double w_current);
+        void UpdatePIDCoefficients(double& Kp, double& Ki, double& Kd, double& ctrl_max);
 
         public:
 
--- src/controller.cpp
+++ src/controller.cpp
@@ -4,7 +4,8 @@ namespace controller
 {
 
     Controller::Controller(ros::NodeHandle& nh) 
-        : nh_(nh), marker_id(0), d_error(0), i_error(0), prev_error(0), prev_time(0), docking{false}, path_received{false}
+        : nh_(nh), marker_id(0), steering_d_error(0), steering_i_error(0), steering_prev_error(0),velocity_d_error(0), velocity_i_error(0), 
+        velocity_prev_error(0), prev_time(0), docking(false), path_received(false)
     {
         if (!ReadParameters())
         {
@@ -25,26 +26,45 @@ namespace controller
 
     bool Controller::ReadParameters()
     {
-        if (!nh_.getParam("pid_coefficients/Kp", Kp)) return false;
-        if (!nh_.getParam("pid_coefficients/Ki", Ki)) return false;
-        if (!nh_.getParam("pid_coefficients/Kd", Kd)) return false;
+        if (!nh_.getParam("pid_coefficients/Kp_max", Kp_max)) return false;
+        if (!nh_.getParam("pid_coefficients/Ki_max", Ki_max)) return false;
+        if (!nh_.getParam("pid_coefficients/Kd_max", Kd_max)) return false;
+        if (!nh_.getParam("pid_coefficients/Kp_min", Kp_min)) return false;
+        if (!nh_.getParam("pid_coefficients/Ki_min", Ki_min)) return false;
+        if (!nh_.getParam("pid_coefficients/Kd_min", Kd_min)) return false;
+
         if (!nh_.getParam("ctrl/ctrl_index", ctrl_index)) return false;
-        if (!nh_.getParam("ctrl/ctrl_max", ctrl_max)) return false;
+        if (!nh_.getParam("ctrl/high_ctrl_max", high_ctrl_max)) return false;
+        if (!nh_.getParam("ctrl/low_ctrl_max", low_ctrl_max)) return false;
+
+        if (!nh_.getParam("velocity_controller/velocity/max_velocity", max_velocity)) return false;
+        if (!nh_.getParam("velocity_controller/velocity/low_velocity", low_velocity)) return false;
+        if (!nh_.getParam("velocity_controller/velocity/high_velocity", high_velocity)) return false;
+        if (!nh_.getParam("velocity_controller/curvature/curvature_coefficient", curvature_coefficient)) return false;
+        if (!nh_.getParam("velocity_controller/curvature/curvature_index", curvature_index)) return false;
+        if (!nh_.getParam("velocity_controller/curvature/curvature_lookahead", curvature_lookahead)) return false;
+        if (!nh_.getParam("velocity_controller/curvature/max_curvature", max_curvature)) return false;
+
         if (!nh_.getParam("wheelbase", wheelbase)) return false;
-        if (!nh_.getParam("velocity", velocity)) return false;
+        if (!nh_.getParam("herhangi_bir_sey", herhangi_bir_sey)) return false;
+
         if (!nh_.getParam("logs/input_log", input_log)) return false;
         if (!nh_.getParam("logs/output_log", output_log)) return false;
+
         if (!nh_.getParam("subscribe_topics/odom_topic", odom_topic)) return false;
         if (!nh_.getParam("subscribe_topics/path_topic", path_topic)) return false;
         if (!nh_.getParam("subscribe_topics/docking_signal_topic", docking_signal_topic)) return false;
         if (!nh_.getParam("publish_topics/cmd_topic", cmd_topic)) return false;
         if (!nh_.getParam("publish_topics/marker_topic", marker_topic)) return false;
-        if (!nh_.getParam("herhangi_bir_sey", herhangi_bir_sey)) return false;
 
-        ROS_INFO("PID Katsayilari: [%f, %f, %f]", Kp, Ki, Kd);
-        ROS_INFO("Control Index: [%d]   Control Max: [%f]", ctrl_index, ctrl_max);
+        if (!nh_.getParam("low_pass_filter/filter_length", filter_length)) return false;
+        if (!nh_.getParam("low_pass_filter/weight_current", weight_current)) return false;
+
+        ROS_INFO("Max PID Katsayilari: [%f, %f, %f]", Kp_max, Ki_max, Kd_max);
+        ROS_INFO("Min PID Katsayilari: [%f, %f, %f]", Kp_min, Ki_min, Kd_min);
+        ROS_INFO("Control Index: [%d]   Control Max: [%f, %f]", ctrl_index, low_ctrl_max, high_ctrl_max);
         ROS_INFO("Dingil Mesafesi: [%f]", wheelbase);
-        ROS_INFO("Arac Hizi: [%f]", velocity);
+        ROS_INFO("Maksimum Arac Hizi: [%f]", max_velocity);
         ROS_INFO("Girdi/Cikti Gosterme: [%d, %d]", input_log, output_log);
 
         return true;
@@ -87,13 +107,15 @@ namespace controller
     {
         vehicle_odom = *msg;
         current_heading = GetYaw(vehicle_odom.pose.pose.orientation);
-        vehicle_odom.pose.pose.position.x += cos(current_heading)*wheelbase*0;
-        vehicle_odom.pose.pose.position.y += sin(current_heading)*wheelbase*0;
+        current_velocity = std::hypot(vehicle_odom.twist.twist.linear.x, vehicle_odom.twist.twist.linear.y);
+        vehicle_odom.pose.pose.position.x += std::cos(current_heading)*wheelbase*0;
+        vehicle_odom.pose.pose.position.y += std::sin(current_heading)*wheelbase*0;
 
         if(input_log)
         {
-            ROS_INFO("Simdiki Konum: [%f, %f]", vehicle_odom.pose.pose.position.x, vehicle_odom.pose.pose.position.y);
-            ROS_INFO("Simdiki Yaw: [%f]", current_heading);
+            ROS_INFO("Mevcut Konum: [%f, %f]", vehicle_odom.pose.pose.position.x, vehicle_odom.pose.pose.position.y);
+            ROS_INFO("Mevcut Yaw: [%f]", current_heading);
+            ROS_INFO("Mevcut Hiz [%f]", current_velocity);
         }
 
         if (path.poses.size() == 0 || docking)
@@ -120,12 +142,13 @@ namespace controller
 
     void Controller::ControlOutput()
     {
+        UpdatePIDCoefficients(Kp, Ki, Kd, ctrl_max);
         geometry_msgs::PoseStamped target_point = ChooseWaypoint(vehicle_odom.pose.pose, path, ctrl_index, ctrl_max);
 
         current_time = ros::Time::now().toSec();
         double dt = current_time - prev_time;
 
-        double steering_angle = PID(UpdateError(target_point.pose, vehicle_odom.pose.pose), Kp, Ki, Kd, dt);
+        double steering_angle = PID(UpdateSteeringError(target_point.pose, vehicle_odom.pose.pose), Kp, Ki, Kd, dt, velocity_prev_error, velocity_i_error, velocity_d_error);
 
         prev_time = current_time;
 
@@ -148,14 +171,17 @@ namespace controller
             ROS_INFO("Donus Acisi: [%f]", steering_angle);
         }
 
+
+        double velocity_cmd = LowPassFilter(CalculateVelocityCmd(max_velocity, CalculatePathCurv(vehicle_odom.pose.pose, path, curvature_index, curvature_lookahead, max_curvature), curvature_coefficient), prev_inputs_deq, filter_length, weight_current);
+
         autoware_msgs::VehicleCmd control_msg;
         control_msg.twist_cmd.twist.angular.z = steering_angle;
-        control_msg.twist_cmd.twist.linear.x = velocity;
+        control_msg.twist_cmd.twist.linear.x = velocity_cmd;
         control_pub.publish(control_msg);
     }
 
 
-    double Controller::PID(double error, double t_Kp, double t_Ki, double t_Kd, double dt)
+    double Controller::PID(double error, double t_Kp, double t_Ki, double t_Kd, double dt, double& prev_error, double& i_error, double& d_error)
     {
         i_error += error * dt;
         if (dt != 0) d_error = (error - prev_error) / dt;
@@ -172,19 +198,18 @@ namespace controller
         int closest_idx = 0;
         int target_idx = closest_idx + ctrl_idx;
 
-        while (!(target_idx<path.poses.size()))
+        if (!(target_idx<path.poses.size())) 
         {
-            target_idx--;
+            target_idx = path.poses.size()-1;
         }
 
         geometry_msgs::PoseStamped target_point = t_path.poses[target_idx];
 
-        while (std::sqrt(std::pow(target_point.pose.position.x-current_point_pose.position.x, 2) + std::pow(target_point.pose.position.y-current_point_pose.position.y, 2)) > ctrl_max)
+        while (std::hypot(target_point.pose.position.x-current_point_pose.position.x, target_point.pose.position.y-current_point_pose.position.y) > ctrl_max)
         {
             if (target_idx>0)
             {
-                target_idx--;
-                target_point = t_path.poses[target_idx];
+                target_point = t_path.poses[--target_idx];
             }
 
             else
@@ -247,7 +272,7 @@ namespace controller
     }
 
 
-    double Controller::UpdateError(const geometry_msgs::Pose& target_point_pose, const geometry_msgs::Pose& current_point_pose)
+    double Controller::UpdateSteeringError(const geometry_msgs::Pose& target_point_pose, const geometry_msgs::Pose& current_point_pose)
     {
         geometry_msgs::Pose transformed_pose = LocalTransform(current_point_pose, target_point_pose);
 
@@ -290,6 +315,111 @@ namespace controller
     double Controller::GetYaw(const geometry_msgs::Quaternion& q)
     {
         return atan2(2.0 * (q.w * q.z + q.x * q.y) , 1.0 - 2.0 * (q.y * q.y + q.z * q.z));
+    }
+
+
+    double Controller::CalculatePathCurv(const geometry_msgs::Pose& current_point_pose, const nav_msgs::Path& t_path, int curv_idx, double curv_lookahead, double max_curv)
+    {
+        while (std::hypot(current_point_pose.position.x - t_path.poses[curv_idx].pose.position.x, 
+            current_point_pose.position.y - t_path.poses[curv_idx].pose.position.y) > curv_lookahead)
+        {
+            if (curv_idx > 0) {
+                curv_idx--;
+            } else break;
+        }
+
+        geometry_msgs::Pose transformed_pose = LocalTransform(current_point_pose, t_path.poses[curv_idx].pose);
+        double dx = transformed_pose.position.x;
+        double dy = transformed_pose.position.y;
+
+/* 
+        double dx = vehicle_odom.pose.pose.position.x - t_path.poses[curv_idx].pose.position.x;
+        double dy = vehicle_odom.pose.pose.position.y - t_path.poses[curv_idx].pose.position.y;
+ */
+
+        double radius = std::abs((std::pow(dx, 2) + std::pow(dy, 2)) / (2*dy));
+
+/*         
+        if (curv_idx < 2) curv_idx = 2;
+        int mid_idx = curv_idx / 2;
+
+        double p1[2] {t_path.poses[0].pose.position.x, t_path.poses[0].pose.position.y};
+        double p2[2] {t_path.poses[mid_idx].pose.position.x, t_path.poses[mid_idx].pose.position.y};
+        double p3[2] {t_path.poses[curv_idx].pose.position.x, t_path.poses[curv_idx].pose.position.y};
+        
+        double a = std::hypot(p2[0] - p3[0], p2[1] - p3[1]);
+        double b = std::hypot(p1[0] - p3[0], p1[1] - p3[1]);
+        double c = std::hypot(p1[0] - p2[0], p1[1] - p2[1]);
+
+        double s = (a + b + c) / 2.0;
+        double area = std::sqrt(s * (s-a) * (s-b) * (s-c));
+
+        if (area < 1e-6) {
+            return 0.0;
+        }
+
+        double radius = (a * b * c) / (4.0 * area);
+ */
+
+        return ((1.0 / radius) < (max_curv) ? (1.0 / radius) : max_curv);
+    }
+
+
+    double Controller::CalculateVelocityCmd(double velocity, double curvature, double k_curvature)
+    {
+        ROS_INFO("Path Curvature: [%f]", curvature);
+        return (velocity/(curvature*k_curvature+1));
+    }
+
+
+    double Controller::LowPassFilter(double input, std::deque<double> &prev_inputs, int f_length, double w_current)
+    {
+        prev_inputs.push_back(input);
+        if (prev_inputs.size() > f_length) prev_inputs.pop_front();
+        if (prev_inputs.size() < 1) return input;
+
+        double sum {0.0};
+
+        for (int i=0; i<(prev_inputs.size()-1); i++) sum+= prev_inputs[i];
+
+        double w_prev = (1.0 - w_current) / (prev_inputs.size()-1);
+
+
+        return ((sum * w_prev) + (input * w_current));
+    }
+
+
+    void Controller::UpdatePIDCoefficients(double& t_Kp, double& t_Ki, double& t_Kd, double& t_ctrl_max)
+    {
+        int fark = high_velocity - low_velocity;
+
+        if (current_velocity<low_velocity+fark*0.25) {
+            t_Kp = Kp_max;
+            t_Ki = Ki_max;
+            t_Kd = Kd_max;
+            t_ctrl_max = high_ctrl_max;
+        } 
+        
+        else if (current_velocity>high_velocity-fark*0.25) {
+            t_Kp = Kp_min;
+            t_Ki = Ki_min;
+            t_Kd = Kd_min;
+            t_ctrl_max = low_ctrl_max;
+        }
+        
+        else
+        {
+            t_Kp = (Kp_max+Kp_min)/2 + (Kp_max-Kp_min)/2*cos(M_PI/(fark*0.5)*(current_velocity-low_velocity-fark*0.25));
+            t_Ki = (Ki_max+Ki_min)/2 + (Ki_max-Ki_min)/2*cos(M_PI/(fark*0.5)*(current_velocity-low_velocity-fark*0.25));
+            t_Kd = (Kd_max+Kd_min)/2 + (Kd_max-Kd_min)/2*cos(M_PI/(fark*0.5)*(current_velocity-low_velocity-fark*0.25));
+            t_ctrl_max = (low_ctrl_max+high_ctrl_max)/2 - (high_ctrl_max-low_ctrl_max)/2*cos(M_PI/(fark*0.5)*(current_velocity-low_velocity-fark*0.25));;
+        }
+
+        if (output_log)
+        {
+            ROS_INFO("Guncel PID Katsayilari: [%f, %f, %f]", t_Kp, t_Ki, t_Kd);
+            ROS_INFO("Guncel Ctrl Max: [%f]", ctrl_max);
+        }
     }
 
 }
